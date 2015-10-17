25771a6526fe9ea2c41d9abc80fd7a4d3550ee4b
Rodolfo Cam <r.cam@arcanys.com>
2015-10-18 03:37:27 +0800

    Adjustments.

--- src/UserBundle/Controller/DashboardController.php
+++ src/UserBundle/Controller/DashboardController.php
@@ -5,6 +5,13 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\Points;
+use UserBundle\Entity\PointsGiven;
+use UserBundle\Entity\PointsLog;
+use UserBundle\Entity\PointsReceived;
+use UserBundle\Entity\PointsReceivedAchievement;
+use UserBundle\Entity\PointsReceivedPending;
+use UserBundle\Entity\User;
 
 class DashboardController extends Controller
 {
@@ -16,6 +23,18 @@ class DashboardController extends Controller
 	public function sendpointsAction(Request $request)
 	{
 		$post = $request->request->all();
+		var_dump($post);exit;
+		$user = $this->searchUserByEmail($post['email']);
+		$u = new User();
+		$u->setPointsGiven($post['points']);
 		
+	}
+	
+	public function searchUserByEmail($email)
+	{
+		$u = new User();
+		$user = $u->findOneByEmail($email);
+		
+		return $user;
 	}
 }
